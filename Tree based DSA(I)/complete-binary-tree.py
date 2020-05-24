7a4414f758b4fabf599f365fd3e23ba957c415c3
Shreevallabha A <60250051+asv0018@users.noreply.github.com>
2020-05-24 20:24:36 +0530

    check this

---
+++ Tree based DSA(I)/complete-binary-tree.py
@@ -0,0 +1,10 @@
+#the main difference between complete binary tree
+#and full binary tree is that,complete binary tree
+#should have all the leaf element to left
+#full binary tree is expected to have either 2 childrens
+#or no children.
+
+#therefore the difference lies here all the complete
+#binary tree need not be a full binary tree and viceversa
+
+prr

befb9ec3c8af3db091d911f5039a6028ba7cfec6
Shreevallabha A <60250051+asv0018@users.noreply.github.com>
2020-05-24 20:47:54 +0530

    complete binary tree

--- Tree based DSA(I)/complete-binary-tree.py
+++ Tree based DSA(I)/complete-binary-tree.py
@@ -6,3 +6,32 @@
 
 #therefore the difference lies here all the complete
 #binary tree need not be a full binary tree and viceversa
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = self.right = None
+
+def isComplete(root,index,numberNodes):
+    if root is None:
+        return True
+    if index >= numberNodes:
+        return False
+    return(isComplete(root.left,2*index + 1,numberNodes) and isComplete(root.right,2*index + 2,numberNodes))
+
+def nodeCount(root):
+    if root is None:
+        return 0
+    return (1 + nodeCount(root.left) + nodeCount(root.right))
+
+root = Node(1)
+root.right = Node(2)
+root.left = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+
+if isComplete(root,0,nodeCount(root)):
+    print("The tree is a complete binary tree")
+else:
+    print("The tree is not a complete binary tree")
